4e772b2279385d858b3a10f56b73b1fc5b6a63ea
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2024-02-25 14:40:42 +0900

    add input component

---
+++ src/components/input.rs
@@ -0,0 +1,22 @@
+use yew::{AttrValue, function_component, Html, html, Properties};
+
+#[derive(Properties, PartialEq)]
+pub struct Props {
+    pub label: AttrValue,
+    pub name: AttrValue,
+    pub input_type: AttrValue,
+}
+
+#[function_component(Input)]
+pub fn input(props: &Props) -> Html {
+    let html_id = format!("edit-{}", props.name);
+    html! {
+        <label id={html_id.clone()}>{props.label.clone()}
+            <input
+                id={html_id}
+                type={props.input_type.clone()}
+                name={props.name.clone()}
+            />
+        </label>
+    }
+}
