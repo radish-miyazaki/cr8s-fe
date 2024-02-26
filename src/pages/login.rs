5cff4a96c7bfb74b1bc3e83418e49c3518b01d22
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2024-02-26 20:44:00 +0900

    replace LoginForm component

--- src/pages/login.rs
+++ src/pages/login.rs
@@ -1,6 +1,6 @@
-use yew::{function_component, Html, html};
+use yew::prelude::*;
 
-use crate::components::input::*;
+use crate::components::login_form::*;
 
 #[function_component(Login)]
 pub fn login() -> Html {
@@ -8,15 +8,7 @@ pub fn login() -> Html {
         <div class="container">
             <div class="row min-vh-100 justify-content-center align-items-center">
                 <div div="col-md-4">
-                    <form>
-                        <div class="mb-3">
-                            <Input input_type="text" name="ユーザ名" label="ユーザ名" />
-                        </div>
-                        <div class="mb-3">
-                            <Input input_type="password" name="パスワード" label="パスワード" />
-                        </div>
-                        <button type="submit">{"ログイン"}</button>
-                    </form>
+                    <LoginForm />
                 </div>
             </div>
         </div>
