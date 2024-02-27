c5b6efe1ef68c1837d90610cbe9fce5b8dd8f8c8
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2024-02-27 20:56:47 +0900

    define routing

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,38 @@ mod components;
 mod pages;
 
 use yew::prelude::*;
+use yew_router::{BrowserRouter, Routable, Switch};
+
+#[derive(Routable, PartialEq, Clone)]
+enum Route {
+    #[at("/")]
+    Home,
+    #[at("/rustaceans")]
+    Rustaceans,
+    #[at("/crates")]
+    Crates,
+    #[at("/login")]
+    Login,
+    #[not_found]
+    #[at("/404")]
+    NotFound,
+}
+
+fn switch(route: Route) -> Html {
+    match route {
+        Route::Home => html! { <pages::home::Home /> },
+        Route::Login => html! { <pages::login::Login /> },
+        Route::NotFound => html! { <pages::not_found::NotFound /> },
+        _ => html! { <pages::login::Login /> },
+    }
+}
 
 #[function_component(App)]
 fn app() -> Html {
     html! {
-        <pages::login::Login />
+        <BrowserRouter>
+            <Switch<Route> render={switch} />
+        </BrowserRouter>
     }
 }
 
