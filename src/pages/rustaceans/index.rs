fc92aa6ff637bc59a1cf4ab372881e2d56de1203
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2024-03-01 16:51:29 +0900

    add some pages related to rustacean

--- src/pages/rustaceans/index.rs
+++ src/pages/rustaceans/index.rs
@@ -1,10 +1,11 @@
 use yew::prelude::*;
 
 use crate::components::header::Header;
+use crate::components::rustcean_list::RustceanList;
 use crate::components::sidebar::Sidebar;
 
-#[function_component(Home)]
-pub fn home() -> Html {
+#[function_component(Rustaceans)]
+pub fn rustaceans() -> Html {
     html! {
         <div class="container">
             <div class="row">
@@ -13,7 +14,7 @@ pub fn home() -> Html {
                 </div>
                 <div class="col mt-3">
                     <Header />
-                    {"Have a great day!"}
+                    <RustceanList />
                 </div>
             </div>
         </div>
