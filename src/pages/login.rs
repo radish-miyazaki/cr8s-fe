c25823531a731786594889cb8bb493e11518d6fa
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2024-03-01 14:17:29 +0900

    redirect home page when logged in

--- src/pages/login.rs
+++ src/pages/login.rs
@@ -1,16 +1,29 @@
 use yew::prelude::*;
+use yew_router::components::Redirect;
 
-use crate::components::login_form::*;
+use crate::{components::login_form::*, contexts::CurrentUserContext, Route};
 
 #[function_component(Login)]
 pub fn login() -> Html {
-    html! {
-        <div class="container">
-            <div class="row min-vh-100 justify-content-center align-items-center">
-                <div div="col-md-4">
-                    <LoginForm />
+    let current_user_context =
+        use_context::<CurrentUserContext>().expect("Current user context is missing.");
+
+    match &current_user_context.user {
+        Some(_) => {
+            html! {
+                <Redirect<Route> to={Route::Home} />
+            }
+        }
+        None => {
+            html! {
+                <div class="container">
+                    <div class="row min-vh-100 justify-content-center align-items-center">
+                        <div div="col-md-4">
+                            <LoginForm />
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
+            }
+        }
     }
 }
