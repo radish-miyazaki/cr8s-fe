dca40be84d50ad506b36901809ca5fc212319dfe
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2024-03-02 21:45:38 +0900

    refactor rustacean pages

--- src/pages/rustaceans/add.rs
+++ src/pages/rustaceans/add.rs
@@ -1,44 +1,22 @@
-use yew::platform::spawn_local;
 use yew::prelude::*;
-use yew_router::prelude::*;
 
-use crate::api::rustaceans::api_rustaceans;
 use crate::components::header::Header;
 use crate::components::rustacean_form::RustaceanForm;
 use crate::components::sidebar::Sidebar;
-use crate::contexts::CurrentUserContext;
-use crate::Route;
 
 #[function_component(RustaceansAdd)]
 pub fn rustaceans_add() -> Html {
-    let current_user_context =
-        use_context::<CurrentUserContext>().expect("Current user context is missing.");
-
-    match &current_user_context.token {
-        Some(token) => {
-            let cloned_token = token.clone();
-            spawn_local(async move {
-                api_rustaceans(&cloned_token).await;
-            });
-
-            html! {
-                <div class="container">
-                    <div class="row">
-                        <div class="col-sm-auto">
-                            <Sidebar />
-                        </div>
-                        <div class="col mt-3">
-                            <Header />
-                            <RustaceanForm />
-                        </div>
-                    </div>
+    html! {
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-auto">
+                    <Sidebar />
                 </div>
-            }
-        }
-        None => {
-            html! {
-                <Redirect<Route> to={Route::Login} />
-            }
-        }
+                <div class="col mt-3">
+                    <Header />
+                    <RustaceanForm />
+                </div>
+            </div>
+        </div>
     }
 }
--- src/pages/rustaceans/index.rs
+++ src/pages/rustaceans/index.rs
@@ -1,22 +1,39 @@
 use yew::prelude::*;
+use yew_router::prelude::*;
 
 use crate::components::header::Header;
 use crate::components::rustcean_list::RustceanList;
 use crate::components::sidebar::Sidebar;
+use crate::contexts::CurrentUserContext;
+use crate::Route;
 
 #[function_component(Rustaceans)]
 pub fn rustaceans() -> Html {
-    html! {
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-auto">
-                    <Sidebar />
+    let current_user_context =
+        use_context::<CurrentUserContext>().expect("Current user context is missing.");
+
+    let fallback = html! { <p> {"Loading..."} </p> };
+
+    match &current_user_context.token {
+        Some(token) => {
+            html! {
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-auto">
+                            <Sidebar />
+                        </div>
+                        <div class="col mt-3">
+                            <Header />
+                            <Suspense {fallback}>
+                                <RustceanList token={token.clone()} />
+                            </Suspense>
+                        </div>
+                    </div>
                 </div>
-                <div class="col mt-3">
-                    <Header />
-                    <RustceanList />
-                </div>
-            </div>
-        </div>
+            }
+        }
+        None => html! {
+            <Redirect<Route> to={Route::Login} />
+        },
     }
 }
